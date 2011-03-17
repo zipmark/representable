f31447cfa4fd0ba3cac063b4b88a13beb6ed4fac
Nick Sutterer <apotonick@gmail.com>
2011-03-17 11:53:15 +0100

    added bundler.

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source :rubygems
+
+gem "activesupport", "~> 3.0.0"
+gem "i18n"
+gem "nokogiri"
+gem "rspec"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,5 @@
-require 'rubygems'
+require 'bundler'
+Bundler.setup
 require 'pathname'
 require_relative './../test/support/fixtures'
 require_relative './../lib/roxml'
-require_relative './shared_specs'
-
-def xml_for(name)
-  Pathname.new(File.dirname(__FILE__)).expand_path.dirname.join("examples/xml/#{name}.xml")
-end
-
-class RoxmlObject
-  include ROXML
-end
