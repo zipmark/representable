80b87040208526dc10c5c972a5eaed39c38f3316
Nick Sutterer <apotonick@gmail.com>
2011-11-29 09:37:30 +0100

    removing .from.

--- lib/representable/nokogiri_extensions.rb
+++ lib/representable/nokogiri_extensions.rb
@@ -4,16 +4,4 @@ Nokogiri::XML::Node.class_eval do
   def add_node(name)
     add_child Nokogiri::XML::Node.new(name, document)
   end
-  
-  # FIXME: remove switch. where is #from used with nodes?
-  def self.from(data)
-    case data
-    when Nokogiri::XML::Node
-      data
-    when Nokogiri::XML::Document
-      data.root
-    else
-      Nokogiri::XML(data).root
-    end
-  end
 end
